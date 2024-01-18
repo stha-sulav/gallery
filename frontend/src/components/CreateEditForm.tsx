6905e5f1816f21e7ba7e305c8b41299d64e8ca69
Sulav Shrestha <sulavshrestha2323@gmail.com>
2024-01-18 20:22:27 +0545

    fix: removed shad/cn ui

--- frontend/src/components/CreateEditForm.tsx
+++ frontend/src/components/CreateEditForm.tsx
@@ -1,72 +1,62 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { PostFormSchema, PostFormProps } from "@/schema/postForm-schema";
-import { Link } from "react-router-dom";
+import { PostFormProps } from "@/schema/postForm-schema";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const CreateEditForm = () => {
-  const form = useForm<PostFormProps>({
-    resolver: zodResolver(PostFormSchema),
-    defaultValues: {
-      caption: "",
-      photos: "",
-    },
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostFormProps>();
 
-  const onSubmit = (data: any) => {
+  // console.log(watch("caption"));
+
+  const handleChange = (e: any) => {
+    console.log(e.target);
+  };
+
+  const onSubmit: SubmitHandler<PostFormProps> = (data) => {
     console.log(data);
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className=" w-3/5 mx-auto my-12 space-y-8"
-      >
-        <FormField
-          control={form.control}
-          name="caption"
-          render={({ field }) => (
-            <FormItem className="flex items-center justify-between gap-8">
-              <FormLabel className="text-lg font-medium">Caption</FormLabel>
-              <FormControl>
-                <Input type="text" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="photos"
-          render={({ field }) => (
-            <FormItem className="flex items-center justify-between gap-8">
-              <FormLabel className="text-lg font-medium">Photos</FormLabel>
-              <FormControl>
-                <Input type="file" {...field} multiple />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="p-4 flex justify-center items-center gap-10">
-          <Button type="submit">Submit</Button>
-          <Link to="/">
-            <Button variant={"outline"}>Cancle</Button>
-          </Link>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="m-auto h-[80vh] flex justify-center text-center"
+    >
+      <div className="w-3/4 mt-8 p-10 flex flex-col gap-8">
+        {/* register your input into the hook by invoking the "register" function */}
+
+        <div className="w-full flex gap-5 items-center p-2">
+          <label htmlFor="caption" className="w-1/6 text-xl">
+            Captions
+          </label>
+          <input
+            id="caption"
+            {...register("caption")}
+            className="w-4/5 border-2 border-black px-3 py-1 rounded-md"
+          />
         </div>
-      </form>
-    </Form>
+
+        <div className="w-full flex gap-5 items-center p-2">
+          <label htmlFor="photos" className="w-1/6 text-xl">
+            Photos
+          </label>
+          <input
+            id="photos"
+            {...register("photos", { required: true })}
+            className=" w-4/5 border-2 border-black px-3 py-1 rounded-md"
+            type="file"
+            onChange={handleChange}
+            multiple
+          />
+        </div>
+
+        {errors.photos && (
+          <span className="text-red-500">Select 1 or upto 8 photos.</span>
+        )}
+        <input type="submit" />
+      </div>
+    </form>
   );
 };
 
--- frontend/src/schema/postForm-schema.tsx
+++ frontend/src/schema/postForm-schema.tsx
@@ -1,17 +1,8 @@
 import { z } from "zod";
 
-export const PostFormSchema = z
-  .object({
-    caption: z.string(),
-    photos: z.string(),
-    // .array(z.custom())
-    // .min(1, {
-    //   message: "No photo to post. Please select 1 or upto 8 photos.",
-    // })
-    // .max(8, {
-    //   message: "No photo to post. Please select 1 or upto 8 photos.",
-    // }),
-  })
-  .partial();
+export const PostFormSchema = z.object({
+  caption: z.string().optional(),
+  photos: z.any(),
+});
 
 export type PostFormProps = z.infer<typeof PostFormSchema>;
